e07ae99b9a5040b7be569bb219d81b193e7595a6
Gabriel Santos <Gabriel.developer.santos@gmail.com>
2020-01-19 17:22:41 -0300

    Primeiro commit

---
+++ Course/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bobo ", 100.00, 2000);
+            account.Balance = 200;
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
